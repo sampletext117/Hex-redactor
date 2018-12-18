ed28a565ee23f431fed7d714b6cb8813af2f250b
sampletext117 <taskforce1@yandex.ru>
2018-12-18 22:55:19 +0300

    Добавлено изменение шривтов и цвета текстового поля

--- Hex-redactor1.0.py
+++ Hex-redactor1.0.py
@@ -4,6 +4,7 @@ from PyQt5 import QtCore, QtGui
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QWidget, QMainWindow
 from PyQt5.QtWidgets import QFileDialog, QPlainTextEdit
+from PyQt5.QtWidgets import QColorDialog, QInputDialog, QFontDialog
 import binascii
 
 
@@ -73,42 +74,77 @@ class MyWidget(QMainWindow):
         self.action_3.setShortcut("Ctrl+O")
         self.action_3.setStatusTip('Открытие выбранного файла')
 
+
         self.action_4.triggered.connect(self.save_file)
         self.action_4.setShortcut("Ctrl+S")
         self.action_4.setStatusTip('Сохранение файла')
+
+        self.action_2.triggered.connect(self.change_background)
+        self.action_2.setShortcut("Alt+F")
+        self.action_2.setStatusTip('Изменение цвета текстового поля')
+
+        self.action_5.triggered.connect(self.change_font_and_size)
+        self.action_5.setShortcut("Alt+R")
+        self.action_5.setStatusTip('Изменение шрифта')
+
+
 
         self.hex_text = MyTextEdit(self)
         self.hex_text.move(250, 32)
         self.hex_text.resize(500, 500)
         self.hex_text.textChanged.connect(self.hex_text_input)
-        self.hex_text.setStyleSheet("background-color: rgb(204, 204, 204);\n"
-                                      "border-color: rgb(215, 215, 215);"
-                                    'font: 10pt "Times New Roman";')
+
 
         self.plain_text = MyTextEdit(self)
         self.plain_text.move(20, 32)
         self.plain_text.resize(200, 500)
         self.plain_text.textChanged.connect(self.plain_text_input)
-        self.plain_text.setStyleSheet("background-color: rgb(204, 204, 204);\n"
-                                                "border-color: rgb(215, 215, 215);"
-                                      'font: 15pt "Times New Roman";')
+
+    def change_background(self):
+        color = QColorDialog.getColor()
+        if color.isValid():
+            self.hex_text.setStyleSheet(
+                "background-color: {}".format(color.name())
+            )
+        if color.isValid():
+            self.plain_text.setStyleSheet(
+                "background-color: {}".format(color.name())
+            )
+
+    def change_font_and_size(self):
+        try:
+            font, ok = QFontDialog.getFont()
+            if ok:
+                self.hex_text.setFont(font)
+                self.plain_text.setFont(font)
+
+        except:
+            pass
+
 
     def open_file(self):
         file_path = QFileDialog.getOpenFileName(self)
-        with open(file_path[0], mode="rb") as f:
-            read_data = f.read()
-            f.close()
-        hexdata = read_data.hex()
-        splited_hex = ' '.join([hexdata[i:i + 2] for i in range(0, len(hexdata), 2)])
-        self.hex_text.setPlainText(splited_hex)
-        b = bytes.fromhex(hexdata)
-        self.plain_text.setPlainText(b.decode("utf8"))
+        if file_path[0] == '':
+            pass
+        else:
+            with open(file_path[0], mode="rb") as f:
+                read_data = f.read()
+                f.close()
+            hexdata = read_data.hex()
+            splited_hex = ' '.join([hexdata[i:i + 2] for i in range(0, len(hexdata), 2)])
+            self.hex_text.setPlainText(splited_hex)
+            b = bytes.fromhex(hexdata)
+            self.plain_text.setPlainText(b.decode("utf8"))
+
 
     def save_file(self):
         file_path = QFileDialog.getSaveFileName(self)
-        with open(file_path[0], mode="wb") as f:
-            f.write(self.get_hex_bytes())
-            f.close()
+        if file_path[0] == '':
+            pass
+        else:
+            with open(file_path[0], mode="rb") as f:
+                read_data = f.read()
+                f.close()
 
     def get_hex_bytes(self):
         b_text = ''.join(self.hex_text.toPlainText().split())
